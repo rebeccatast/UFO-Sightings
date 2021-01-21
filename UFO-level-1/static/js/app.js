89fbc0443942a29a591f2743b71d0613081ce2d4
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2021-01-21 13:35:15 -0600

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
-var tableData = data;
+showTable(data);
+d3.select('button').on('click',handleClick);
+function showTable(data) {
+    d3.select('tbody').html('');
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val => {
+            var cell = row.append('td');
+            cell.text(val);
+        });
+    });
+};
+//Filter Function
+function handleClick() {
+    var filteredData = data;
+    var value = d3.select('input').property('value');
+    if (value) {
+        filteredData = filteredData.filter(obj => obj.datetime == value);
+    };
+    d3.select('input').property('value','');
+    showTable(filteredData);
+};
 
-// YOUR CODE HERE!
