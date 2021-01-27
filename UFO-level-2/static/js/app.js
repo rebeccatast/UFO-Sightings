fc92e4b5742d58cf5708af1d322d8b2721a6a1ec
Rebecca Tast <69356535+rebeccatast@users.noreply.github.com>
2021-01-26 19:27:51 -0600

    Update app.js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,75 +1,47 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// Display the entire dataset as default
-
-tableData.forEach((report) => {
-    console.log(report);
-    var row = tbody.append('tr');
-
-    Object.entries(report).forEach(([key, value]) => {
-        console.log(key, value);
-        var cell = row.append('td');
-        cell.text(value);
+//Display dataset
+showTable(data);
+d3.select('button').on('click',handleClick);
+function showTable(data) {
+    d3.select('tbody').html('');
+    data.forEach(obj => {
+        var row = d3.select('tbody').append('tr');
+        Object.values(obj).forEach(val => {
+            var cell = row.append('td');
+            cell.text(val);
+        });
     });
-});
+};
 
-// Select the submit button in the html file
-var clickHandler = d3.select("#filter-btn");
-
-//Click event
-clickHandler.on("click", function() {
-
-//Remove existing table
-d3.select("tbody").html("");
+//Filter dataset
+function handleClick() {
+    var filteredData = data;
+    var dateTime = d3.select("#datetime").property("value");
+    var selectedCountry = d3.select("#country").property("value").toLowerCase();
+    var selectedState = d3.select("#state").property("value").toLowerCase();
+    var selectedCity = d3.select("#city").property("value").toLowerCase();
+    var selectedShape = d3.select("#shape").property("value").toLowerCase();
+    if (dateTime) {
+        filteredData = filteredData.filter(record => record.datetime === dateTime);
+    }
+    if (selectedCountry) {
+        filteredData = filteredData.filter(record => record.country === selectedCountry);
+    }
+    if (selectedState) {
+        filteredData = filteredData.filter(record => record.state === selectedState);
+    }
+    if (selectedCity) {
+        filteredData = filteredData.filter(record => record.city === selectedCity);
+    }
+    if (selectedShape) {
+        filteredData = filteredData.filter(record => record.shape === selectedShape);
+    };
+    //Show filtered data
+    d3.select('input').property('value','');
+    showTable(filteredData);
+};
 
 //Prevent page from refreshing
 d3.event.preventDefault();
-
-// Get the value property of the input elements and set all text to lowercase
-var dateTime = d3.select("#datetime").property("value");
-
-var selectedCountry = d3.select("#country").property("value").toLowerCase();
-
-var selectedState = d3.select("#state").property("value").toLowerCase();
-
-var selectedCity = d3.select("#city").property("value").toLowerCase();
-
-var selectedShape = d3.select("#shape").property("value").toLowerCase();
-
-// initialize tableData as filteredData
-filteredData = tableData;
-
-if (dateTime) {
-    filteredData = filteredData.filter(record => record.datetime === dateTime);
-}
-if (selectedCountry) {
-    filteredData = filteredData.filter(record => record.country === selectedCountry);
-}
-if (selectedState) {
-    filteredData = filteredData.filter(record => record.state === selectedState);
-}
-if (selectedCity) {
-    filteredData = filteredData.filter(record => record.city === selectedCity);
-}
-if (selectedShape) {
-    filteredData = filteredData.filter(record => record.shape === selectedShape);
-}
-
-// Display the filtered dataset
-
-filteredData.forEach((report) => {
-    var row = tbody.append('tr');
-
-    Object.entries(report).forEach(([key, value]) => {
-        console.log(key, value);
-        var cell = row.append('td');
-        cell.text(value);
-    });
-});
-});
-
-
